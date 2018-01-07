6f440787c06f47cdfa97a559c1470dac3325804a
639dev <gharood_3@hotmail.com>
2018-01-08 01:49:38 +0300

    refactor js by implementing OOP

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
-var todos = ['item1','item2','item3'];
-
-// It should display all todos
-function displayTodos(){
-  console.log('My todos:'.todos);
-}
-
-// It should add a todo
-function addTodo(todo){
-  todos.push(todo);
-  displayTodos();
-}
-
-// It should change a todo
-function changeTodo(position,newValue){
-  todos[position] = newValue;
-  displayTodos();
-}
-
-// It should delete a todo
-function deleteTodo(position){
-  todos.splice(position,1);
-  displayTodos();
-}
+var todoList = {
+    todos: ['item1', 'item2', 'item3'],
+    displayTodos: function() {
+        console.log('My todos:',this.todos)
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
